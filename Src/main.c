15fb56bb88ddf81a18b7a7ff9b07821faa30ebd0
Ildar Mulyukov <ildar@altlinux.ru>
2017-01-31 15:46:20 +0600

    Blink sample program

--- Src/main.c
+++ Src/main.c
@@ -95,7 +95,8 @@ int main(void)
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
-
+	HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin); //Toggle the state of pin PC13
+	HAL_Delay(500);
   }
   /* USER CODE END 3 */
 
