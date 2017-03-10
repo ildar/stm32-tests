1c7f0088402d744bee05f21e54638c60b2da59cd
Ildar Mulyukov <ildar@altlinux.ru>
2017-03-10 18:17:04 +0600

    CGreen sample

--- Src/_main.c
+++ Src/_main.c
@@ -1,7 +1,8 @@
-#include <string.h>
 #include "main.h"
 #include "stm32f1xx_hal.h"
 #include "stm32f1xx_hal_gpio.h"
+#include <string.h>
+#include <stdarg.h>
 
 extern UART_HandleTypeDef huart1;
 extern PCD_HandleTypeDef hpcd_USB_FS;
@@ -10,25 +11,30 @@ extern int test_main (int argc, const char* argv[]);
 
 static const char* STR_GREET1 = "\r\nHello, UART user!\r\n--\r\n";
 
-void print(char* s) { // blinks while printnig to UART
+int printf(const char* format, ...) { // blinks while printnig to UART
+	va_list args;
+	int sz;
+	char prbuf[80];
 	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
-	HAL_UART_Transmit(&huart1, (uint8_t*) s, strlen(s), HAL_MAX_DELAY);
+	va_start( args, format );
+	sz=vsnprintf(prbuf, sizeof(prbuf), format, args);
+	HAL_UART_Transmit(&huart1, (uint8_t*)prbuf, (sz>80)?80:sz, HAL_MAX_DELAY);
+	va_end( args );
 	HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
 }
-void stdimpl_print(const char *string) { print(string); }
 
 void _main(void)
 {
 	uint8_t c;
 
-	print(STR_GREET1);
+	printf(STR_GREET1);
 
 	while ( HAL_UART_Receive(&huart1, &c, 1, 500) ) {
-		print(".");
+		printf(".");
 	}
 
 	while(1) {
-		print("\n\r");
+		printf("\n\r");
 		test_main(0, NULL);
 		HAL_Delay(5000);
 	}
